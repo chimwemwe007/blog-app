5ebe248ae05b1f3efe1b275122357b1f6c04bc1a
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-10-28 16:10:07 +0200

    Add CommentsCounter to be an integer greater than or equal to zero.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :users, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :posts, foreign_key: 'post_id', class_name: 'Post'
   after_save :update_comments_counter
 
-  private
+  validates :text, presence: true
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    posts.update(comments_counter: posts.comments_counter + 1)
   end
 end
