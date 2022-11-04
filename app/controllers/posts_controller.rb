b68f43162019b962d86cd09dfe59dcc13e82c029
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-11-04 22:23:46 +0200

    Creates a Post on behalf of the current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,33 @@ class PostsController < ApplicationController
     puts params
     @post = Post.find(params[:id])
   end
+
+  # form actions
+  def new
+    @user = User.find(current_user.id)
+    @post = @user.post.new
+    render :new, locals: { post: @post }
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.post.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post created successfully'
+          redirect_to "#{user_posts_path(@user)}/#{@post.id}"
+        else
+          flash[:error] = 'Post not created'
+          render :new, locals: { post: @post }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:new_post).permit(:title, :text)
+  end
 end
